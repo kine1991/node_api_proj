20a9a7a4ab22ea536cded20ecc56763f8293c73b
kine1991 <kine4599@gmail.com>
2019-09-18 21:17:18 +0300

    added file for send data to database

--- data/dataSendToDB.js
+++ data/dataSendToDB.js
@@ -1,15 +1,67 @@
 const mongoose = require("mongoose")
 const fs = require("fs")
 const dotenv = require('dotenv');
-const data = require('./data.json');
+const Car = require('../models/carModels');
+
 
 dotenv.config({ path: './config.env' });
-
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 
-console.log(DB)
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => console.log('DB connection successful!'))
 
-const car = JSON.parse(fs.readFileSync(`${__dirname}/data.json`), 'utf-8')
+
+const cars = JSON.parse(fs.readFileSync(`${__dirname}/data.json`), 'utf-8')
+
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+    try {
+      await Car.create(cars);
+      console.log('Data successfully loaded!');
+    } catch (err) {
+      console.log(err);
+    }
+    process.exit();
+  };
+  
+  // DELETE ALL DATA FROM DB
+  const deleteData = async () => {
+    try {
+      await Car.deleteMany();
+      console.log('Data successfully deleted!');
+    } catch (err) {
+      console.log(err);
+    }
+    process.exit();
+  };
+  
+  if (process.argv[2] === '--import') {
+    importData();
+  } else if (process.argv[2] === '--delete') {
+    deleteData();
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // console.log(car)
 
