69d728060ee7d37172c3d3725de2b990d8ebf8d6
kine1991 <kine4599@gmail.com>
2019-09-26 14:28:45 +0300

    create basic user model

---
+++ models/userModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter your name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter your email'],
+        lowercase: true,
+        unique: true,
+        validate: [validator.isEmail, 'Please provide a valid email!']
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, 'Please enter your password'],
+        minlength: true,
+        select: false //не выводиться из базы данных
+    },
+    passwordConfirm: {
+        type: String,
+        validate: {
+            validator: function(el){
+                return el === this.password; // this будеть работать только при сохранении (.create() .save()) update find не подходят
+            },
+            message: 'field confirm passsword do not match the same'
+        }
+    },
+    passwordChangedAt: {
+        type: Date,
+        default: Date.now()
+        // require: [true, 'Please tell us your date!']
+      }
+});
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User;
