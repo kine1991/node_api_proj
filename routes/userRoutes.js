90ae02ff4aa7a38728adce1353d5ad045af926c5
kine1991 <kine4599@gmail.com>
2019-09-30 12:17:54 +0300

    added resetPassword method

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,6 +12,7 @@ router.route('/login').post(authController.login);
 router.route('/:id').delete(authController.protect, userController.deleteUser)
 
 router.route('/forgotPassword').post(authController.forgotPassword);
+router.route('/resetPassword/:token').patch(authController.resetPassword);
 
 
 module.exports = router;
