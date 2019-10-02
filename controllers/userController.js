d6f398e6ca9fcc40d62e0eb62193f304acea8d02
kine1991 <kine4599@gmail.com>
2019-10-02 15:38:37 +0300

    added updateMe method

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,14 @@ const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach(el => {
+      if (allowedFields.includes(el)) newObj[el] = obj[el];
+    });
+    return newObj;
+};
+
 exports.getAllUsers = async (req, res, next) => {
     try{
         const users = await User.find()
@@ -15,7 +23,6 @@ exports.getAllUsers = async (req, res, next) => {
     } catch(err) {
         next(err);
     }
-
 }
 
 exports.deleteUser = async (req, res, next) => {
@@ -29,4 +36,24 @@ exports.deleteUser = async (req, res, next) => {
     } catch(err){
         next(err);
     }
-}
+};
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+    // 1) Create error if user POSTs password data
+    if(req.body.password || req.body.passwordConfirm){
+        return next(new AppError('This route is not for password updates. Please use /updateMyPassword.', 400));
+    }
+    filteredBody = filterObj(req.body, 'name', 'email');
+    // 2) Update user document
+    const updatedUser = await User.findByIdAndUpdate(req.user._id, filteredBody, {
+        new: true,
+        runValidators: true // будет производить валидацию при обновлении
+    });
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updatedUser
+        }
+    });
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,6 +14,6 @@ router.route('/:id').delete(authController.protect, userController.deleteUser)
 router.route('/forgotPassword').post(authController.forgotPassword);
 router.route('/resetPassword/:token').patch(authController.resetPassword);
 router.route('/updateMyPassword').patch(authController.protect, authController.updateMyPassword);
-
+router.route('/updateMe').patch(authController.protect, userController.updateMe)
 
 module.exports = router;
